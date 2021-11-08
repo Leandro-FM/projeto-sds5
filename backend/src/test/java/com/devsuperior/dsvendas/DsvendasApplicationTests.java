6823b7d8169116c7b7dbd9754538d7a67e9f0c58
Leandro Ferreira Machado <lfeletron2@gmail.com>
2021-11-07 22:36:22 -0200

    First homolog

--- backend/src/test/java/com/devsuperior/dsvendas/DsvendasApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dsvendas/DsvendasApplicationTests.java
@@ -3,6 +3,7 @@ package com.devsuperior.dsvendas;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class DsvendasApplicationTests {
 
